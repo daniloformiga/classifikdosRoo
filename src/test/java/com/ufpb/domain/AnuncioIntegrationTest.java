66eca95f5c677f50f4525d94d97a71713b1a5cb6
Danilo Formiga <daniloformiga@gmail.com>
2014-03-31 02:42:00 -0300

    integration tests

--- src/test/java/com/ufpb/domain/AnuncioIntegrationTest.java
+++ src/test/java/com/ufpb/domain/AnuncioIntegrationTest.java
@@ -1,9 +1,30 @@
 package com.ufpb.domain;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
 @RooIntegrationTest(entity = Anuncio.class)
 public class AnuncioIntegrationTest {
+	
+	@Test
+	public void testRelacionamento(){
+		
+		Usuario usu = new Usuario();
+		usu.setNome("testador");
+		usu.setSenha("testador");
+		
+		Produto p = new Produto();
+		p.setNome("testProduto");
+		p.setUsuario(usu);
+		
+		Anuncio a = new Anuncio();
+		a.setNome("testAnuncio");
+		a.setProduto(p);
+		
+		Assert.assertNotNull(p.getUsuario());
+		Assert.assertNotNull(a.getProduto());
+		
+	}
 
     @Test
     public void testMarkerMethod() {
--- src/test/java/com/ufpb/domain/ProdutoIntegrationTest.java
+++ src/test/java/com/ufpb/domain/ProdutoIntegrationTest.java
@@ -1,11 +1,32 @@
 package com.ufpb.domain;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
 @RooIntegrationTest(entity = Produto.class)
 public class ProdutoIntegrationTest {
+	
+	@Test
+	public void testRelacionamento() {
+		
+		Usuario usu = new Usuario();
+		usu.setNome("testador");
+		usu.setSenha("testador");
+		
+		Produto p = new Produto();
+		p.setNome("TestProduto");
+		p.setUsuario(usu);
+		
+		Assert.assertNotNull(p.getUsuario());
+		
+		
+		
+		
+	}
 
     @Test
     public void testMarkerMethod() {
+    	
+    	
     }
 }
